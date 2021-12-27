dce55132a8cc31aeceafb9358acf1e3c720f5671
Ellis Robinson <ellis.robinson@live.co.uk>
2021-12-27 14:31:08 +0000

    add products view and check functionality of products variable

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def all_products(request):
+    ''' a view that shows all products, and allows for searches of products'''
+    
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+
+    return render(request, 'products/products.html', context)
