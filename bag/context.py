c94116541d3e3cbac428b612a4c345c40de56c93
Ellis Robinson <ellis.robinson@live.co.uk>
2022-01-02 13:45:40 +0000

    add item_id, quantity and product variables to context.py

--- bag/context.py
+++ bag/context.py
@@ -1,11 +1,27 @@
+''' has shopping bag contents function for use across whole site '''
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
+    ''' tracks items in shopping bag and updates delivery cost '''
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     if total < settings.FREE_DELIVERY_FRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
         free_delivery_delta = settings.FREE_DELIVERY_FRESHOLD - total
@@ -24,5 +40,5 @@ def bag_contents(request):
         'free_delivery_delta': free_delivery_delta,
         'grand_total': grand_total,
     }
-    
+
     return context
