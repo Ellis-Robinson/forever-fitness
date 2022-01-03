3fa319c5dcb9d9224ae836adc8027edb375187ac
Ellis Robinson <ellis.robinson@live.co.uk>
2022-01-03 18:02:41 +0000

    add adjust bag and remove from bag views

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,5 @@
 ''' stores all functions for bag app '''
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def bag(request):
@@ -33,3 +33,52 @@ def add_to_bag(request, item_id):
     request.session['bag'] = shopping_bag
 
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    ''' adjusts the quantity of item in bag '''
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'sizes' in request.POST:
+        size = request.POST['sizes']
+    shopping_bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            shopping_bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del shopping_bag[item_id]['items_by_size'][size]
+            if not shopping_bag[item_id]['items_by_size']:
+                shopping_bag.pop(item_id)
+    else:
+        if quantity > 0:
+            shopping_bag[item_id] = quantity
+        else:
+            shopping_bag.pop(item_id)
+
+    request.session['bag'] = shopping_bag
+
+    return redirect(reverse('bag'))
+
+
+def remove_from_bag(request, item_id):
+    ''' removes the item from shopping bag '''
+    try:
+        size = None
+        if 'sizes' in request.POST:
+            size = request.POST['sizes']
+        shopping_bag = request.session.get('bag', {})
+
+        if size:
+            del shopping_bag[item_id]['items_by_size'][size]
+            if not shopping_bag[item_id]['items_by_size']:
+                shopping_bag.pop(item_id)
+        else:
+            shopping_bag.pop(item_id)
+
+        request.session['bag'] = shopping_bag
+
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
