27c8642ab9b1c820a4a9aa0ed53d19c281dc0b53
Ellis Robinson <ellis.robinson@live.co.uk>
2022-01-03 12:40:49 +0000

    add logic for item sizes in add to bag function

--- bag/views.py
+++ bag/views.py
@@ -11,10 +11,25 @@ def add_to_bag(request, item_id):
     ''' allows user to add items to bag '''
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'sizes' in request.POST:
+        size = request.POST['sizes']
     shopping_bag = request.session.get('bag', {})
-    if item_id in list(shopping_bag.keys()):
-        shopping_bag[item_id] += quantity
+
+    if size:
+        if item_id in list(shopping_bag.keys()):
+            if size in shopping_bag[item_id]['items_by_size'].keys():
+                shopping_bag[item_id]['items_by_size'][size] += quantity
+            else:
+                shopping_bag[item_id]['items_by_size'][size] = quantity
+        else:
+            shopping_bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        shopping_bag[item_id] = quantity
+        if item_id in list(shopping_bag.keys()):
+            shopping_bag[item_id] += quantity
+        else:
+            shopping_bag[item_id] = quantity
+
     request.session['bag'] = shopping_bag
+
     return redirect(redirect_url)
