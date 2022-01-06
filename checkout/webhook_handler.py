1d29a6e59a8818a390a8954eccf2069ad02dd0a2
Ellis Robinson <ellis.robinson@live.co.uk>
2022-01-06 10:01:06 +0000

    added wh secret and set up function to listen for webhooks

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,13 +1,28 @@
 from django.http import HttpResponse
 
+
 class StripeWH_Handler:
     ''' handle webhooks from stripe'''
 
     def __init__(self, request):
         self.request = request
-    
-    def handler_event(self, event):
+
+    def handle_event(self, event):
         '''handle webhook events'''
+        return HttpResponse(
+            content=f"Unhandled webhook recieved {event['type']}",
+            status=200
+        )
+
+    def handle_payment_intent_succeeded(self, event):
+        '''handle payment intent succeeded event'''
+        return HttpResponse(
+            content=f"webhook recieved {event['type']}",
+            status=200
+        )
+
+    def handle_payment_intent_payment_failed(self, event):
+        '''handle payment intent failed event'''
         return HttpResponse(
             content=f"webhook recieved {event['type']}",
             status=200
