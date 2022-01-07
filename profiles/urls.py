178af1b6d0211844df9c95024178e82d75b6c330
Ellis Robinson <ellis.robinson@live.co.uk>
2022-01-07 15:01:10 +0000

    create user_orders view and added order history to user_orders template

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.user_profile, name='profile'),
-    path('', views.user_orders, name='user_orders')
+    path('user_orders/', views.user_orders, name='user_orders'),
+    path('order_history/<order_number>', views.order_history, name='order_history'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
 
 def user_profile(request):
     ''' renders users profile '''
@@ -37,6 +38,23 @@ def user_orders(request):
     template = 'profiles/user_orders.html'
     context = {
         'orders': orders,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
