2dd221b50200d9277b20a4866bacdab288401d6b
Ellis Robinson <ellis.robinson@live.co.uk>
2022-01-07 11:55:34 +0000

    add container header and some bootstrap styles to all allauth templates.

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
-def profile(request):
+def user_profile(request):
     ''' Open users profile '''
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
     context = {
-
+        'profile': profile
     }
 
     return render(request, template, context)
