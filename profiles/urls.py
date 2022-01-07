109d72ff3614869ed95aa218a065ad9b36b645a9
Ellis Robinson <ellis.robinson@live.co.uk>
2022-01-07 14:08:16 +0000

    added user delivery info to user profile page

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_profile, name='profile')
+    path('', views.user_profile, name='profile'),
+    path('', views.user_orders, name='user_orders')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,42 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def user_profile(request):
-    ''' Open users profile '''
+    ''' renders users profile '''
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile successfully updated")
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
-        'profile': profile
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+def user_orders(request):
+    ''' renders users orders page'''
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    orders = profile.orders.all()
+
+    template = 'profiles/user_orders.html'
+    context = {
+        'orders': orders,
     }
 
     return render(request, template, context)
