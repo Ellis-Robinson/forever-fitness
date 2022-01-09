77f2d2ba8d291fde066e04fc1829273a3e73fe43
Ellis Robinson <ellis.robinson@live.co.uk>
2022-01-09 13:33:45 +0000

    add edit_product view, template and url

--- products/views.py
+++ products/views.py
@@ -94,3 +94,27 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    ''' update product in database '''
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product successfully updated.')
+            return redirect(reverse('product_details', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Is the form valid?')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'you are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
