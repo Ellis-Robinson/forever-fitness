bd68d29fa5c77e6f88cf18480853565c868a2ca0
Ellis Robinson <ellis.robinson@live.co.uk>
2022-01-09 14:11:49 +0000

    add delete product view, url, template and POST method

--- products/views.py
+++ products/views.py
@@ -80,9 +80,9 @@ def add_product(request):
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product successfully added.')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_details', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Is the form valid?')
     else:
@@ -118,3 +118,22 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    ''' Delete a product from database '''
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == "POST":
+
+        product.delete()
+        messages.success(request, 'Product deleted')
+
+        return redirect(reverse('products'))
+
+    else:
+        template = 'products/delete_product.html'
+        context = {
+            'product': product,
+        }
+        return render(request, template, context)
