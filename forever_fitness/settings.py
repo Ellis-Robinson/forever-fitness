6b58a6224099901576d351d18a5c7e3cf2e54444
Ellis Robinson <ellis.robinson@live.co.uk>
2022-01-10 14:43:48 +0000

    created db.json file and loaded to postgres database

--- forever_fitness/settings.py
+++ forever_fitness/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 
@@ -123,6 +124,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
