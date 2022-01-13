92b9ece6ad93f98a6f9e513e2f644078d545cf37
Ellis Robinson <ellis.robinson@live.co.uk>
2022-01-13 16:01:13 +0000

    add trailing slash to media and static urls

--- forever_fitness/settings.py
+++ forever_fitness/settings.py
@@ -189,8 +189,8 @@ if 'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION = 'media'
     
     # overide static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
