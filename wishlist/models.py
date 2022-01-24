f8f4bc35169865b37ecffb411ae9fc1f2e4c0f4a
Ellis Robinson <ellis.robinson@live.co.uk>
2022-01-24 14:22:21 +0000

    change WishListItem model __str__ function to return self.wishlist

--- wishlist/models.py
+++ wishlist/models.py
@@ -24,4 +24,4 @@ class WishListItem(models.Model):
                                 on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.wishlist.profile)
+        return str(self.wishlist)
